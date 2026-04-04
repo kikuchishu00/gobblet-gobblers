7c09d320cbc657e28bb529eebf77ab7c09b258a6
菊地秋 <kikuchishu@kikuchiakis-MacBook-Air.local>
2026-04-04 14:09:13 +0900

    PWA化

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,8 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        navigateFallback: 'index.html',
+        navigateFallbackDenylist: [/^\/api/],
       },
     }),
   ],
